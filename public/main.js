6313b1fcced2c3a65fbf199f4b9c653ca6ff7cd5
Salas, Claudio [ITAS LatAm] <csalas10@its.jnj.com>
2018-04-27 06:13:51 -0300

    corrigido erro em ingles

--- public/main.js
+++ public/main.js
@@ -102,7 +102,7 @@ $(function() {
   // Adds the visual chat typing message
   function addChatTyping (data) {
     data.typing = true;
-    data.message = 'is typing';
+    data.message = 'está digitando';
     addChatMessage(data);
   }
 
@@ -243,13 +243,13 @@ $(function() {
 
   // Whenever the server emits 'user joined', log it in the chat body
   socket.on('user joined', function (data) {
-    log(data.username + ' joined');
+    log(data.username + ' entrou');
     addParticipantsMessage(data);
   });
 
   // Whenever the server emits 'user left', log it in the chat body
   socket.on('user left', function (data) {
-    log(data.username + ' left');
+    log(data.username + ' saiu');
     addParticipantsMessage(data);
     removeChatTyping(data);
   });
